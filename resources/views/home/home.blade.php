f9ac8c826a1cac9f20b4fa7ad3d406f8cef3ff41
Next to seven <huatianyingdou@gmail.com>
2024-06-24 14:53:07 +0900

    add: リスト表示追加

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -1,9 +1,21 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Home Page</title>
+ <title>Home Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <body>
     <h1>Welcome to the Home Page</h1>
+    <?php
+        $subject = ["subject1", "subject2"];
+        foreach ($subject as $v){
+            echo '
+            <div class="h-32 w-32 flex items-center justify-center rounded-md bg-green-400">
+                '. $v.'
+            </div>';
+        }
+    ?>
+
+
 </body>
 </html>
