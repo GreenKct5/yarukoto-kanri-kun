53418f977e62d9ea78a5172b54bc9d765415bbee
Next to seven <huatianyingdou@gmail.com>
2024-07-04 13:29:36 +0900

    add:リスト画面

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -5,35 +5,52 @@
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <body>
-    <h1>Welcome to the Home Page</h1>
-    <div class="h-32 w-64  rounded-md bg-green-800">
-                <b><input type="checkbox">課題内容</b>
-                <p>　・日付，時間</p>
-                <p>　・提出先</p>
-                <p>　・タグ</p>
-                <p>　・コメント</p>
-            </div>
-    <div class="h-32 w-64  rounded-md bg-green-600">
-                <b><input type="checkbox">課題内容</b>
-                <p>　・日付，時間</p>
-                <p>　・提出先</p>
-                <p>　・タグ</p>
-                <p>　・コメント</p>
-            </div>
-    <div class="h-32 w-64  rounded-md bg-green-400">
-                <b><input type="checkbox">課題内容</b>
-                <p>　・日付，時間</p>
-                <p>　・提出先</p>
-                <p>　・タグ</p>
-                <p>　・コメント</p>
-            </div>
-            <div class="h-32 w-64  rounded-md bg-green-200">
-                <b><input type="checkbox">課題内容</b>
-                <p>　・日付，時間</p>
-                <p>　・提出先</p>
-                <p>　・タグ</p>
-                <p>　・コメント</p>
-            </div>
-
+    <h1>&nbspや管くん</h1>
+    <div class="h-auto w-64  rounded-md bg-green-800">
+                <b>&nbsp科目名</b>
+                <div class="h-auto w-60 mx-auto rounded-md bg-green-200">
+                            <div class="flex">&nbsp<input type="checkbox">&nbsp<img src="../../../img/Vector_01.svg">&nbsp<p>課題内容</p></div>
+                            <div class="flex">　&nbsp<img src="../../../img/Vector_02.svg">&nbsp<p>日付，時間</p></div>
+                            <div class="flex">　&nbsp&nbsp<img src="../../../img/Vector_03.svg">&nbsp<p>提出先
+                                <select name="提出先">
+                                <option value="Webclass">Webclass</option>
+                                <option value="Teams">Teams</option>
+                                <option value="授業時">授業時</option>
+                                </select>
+                            </p></div>
+                            <div class="flex">　&nbsp&nbsp<img src="../../../img/Vector_04.svg">&nbsp<p>タグ</p></div>
+                            <div class="flex">　&nbsp<img src="../../../img/Vector_05.svg">&nbsp<p>メモ</p></div>
+                        </div><br/>
+                <div class="h-auto w-60 mx-auto rounded-md bg-green-200">
+                <div class="flex">&nbsp<input type="checkbox">&nbsp<img src="../../../img/Vector_01.svg">&nbsp<p>課題内容</p></div>
+                            <div class="flex">　&nbsp<img src="../../../img/Vector_02.svg">&nbsp<p>日付，時間</p></div>
+                            <div class="flex">　&nbsp&nbsp<img src="../../../img/Vector_03.svg">&nbsp<p>提出先
+                                <select name="提出先">
+                                <option value="Webclass">Webclass</option>
+                                <option value="Teams">Teams</option>
+                                <option value="授業時">授業時</option>
+                                </select>
+                            </p></div>
+                            <div class="flex">　&nbsp&nbsp<img src="../../../img/Vector_04.svg">&nbsp<p>タグ</p></div>
+                            <div class="flex">　&nbsp<img src="../../../img/Vector_05.svg">&nbsp<p>メモ</p></div>
+                        </div><br/>
+            </div><br/>
+    <div class="h-auto w-64  rounded-md bg-green-600">
+                <b>&nbsp科目名</b>
+                <div class="h-auto w-60 mx-auto rounded-md bg-green-200">
+                <div class="flex">&nbsp<input type="checkbox">&nbsp<img src="../../../img/Vector_01.svg">&nbsp<p>課題内容</p></div>
+                            <div class="flex">　&nbsp<img src="../../../img/Vector_02.svg">&nbsp<p>日付，時間</p></div>
+                            <div class="flex">　&nbsp&nbsp<img src="../../../img/Vector_03.svg">&nbsp<p>提出先
+                                <select name="提出先">
+                                <option value="Webclass">Webclass</option>
+                                <option value="Teams">Teams</option>
+                                <option value="授業時">授業時</option>
+                                </select>
+                            </p></div>
+                            <div class="flex">　&nbsp&nbsp<img src="../../../img/Vector_04.svg">&nbsp<p>タグ</p></div>
+                            <div class="flex">　&nbsp<img src="../../../img/Vector_05.svg">&nbsp<p>メモ</p></div>
+                        </div><br/>
+            </div><br/>
+ 
 </body>
 </html>
