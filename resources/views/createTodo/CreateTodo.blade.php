961eeeb67ab9191f53fa88cf7909a1d3ffbdec06
kototo522 <huyejiben@gmail.com>
2024-07-07 19:36:41 +0900

    update: 表示するエラー文を変更

--- resources/views/createTodo/CreateTodo.blade.php
+++ resources/views/createTodo/CreateTodo.blade.php
@@ -12,11 +12,6 @@
         @if ($errors->any())
             <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded mb-4" role="alert">
                 <strong class="font-bold">記入していない箇所があります</strong>
-                <ul class="list-disc pl-5">
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
             </div>
         @endif
 
