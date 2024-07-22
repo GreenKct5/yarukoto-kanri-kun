21098631b1bf91babbf563de7b7928ed11b05bfd
kototo522 <huyejiben@gmail.com>
2024-07-22 15:36:30 +0900

    pintの実行

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Todo;
-use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
         $todos = Todo::all()->groupBy('subject_id');
+
         return view('home.home', compact('todos'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TodoController;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
