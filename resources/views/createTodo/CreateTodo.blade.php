5a45bce8d14e6085cc6eaffcd319de09004e7842
kototo522 <huyejiben@gmail.com>
2024-07-22 09:16:42 +0900

    update: cssの変更

--- resources/views/createTodo/CreateTodo.blade.php
+++ resources/views/createTodo/CreateTodo.blade.php
@@ -5,8 +5,8 @@
     <title>CreateTodo Page</title>
     @vite('resources/css/app.css')
 </head>
-<body class="flex items-center justify-center min-h-screen">
-    <div class="container mx-auto p-4" id="menu">
+<body class="flex items-center justify-center min-h-screen w-full">
+    <div class="container" id="menu">
         @if ($errors->any() && ($errors->has('subject') || $errors->has('task') || $errors->has('deadline') || $errors->has('提出先')))
             <div class="absolute left-3/4 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded -mx-1.5" role="alert">
                 <strong class="font-bold">記入していない箇所があります</strong>
