e7afca291471ac8642808e92b9d351fe1d4fd57c
n00bdy-noname <n00bdy.noname@gmail.com>
2024-07-25 07:43:46 +0900

    update: レスポンシブ対応

--- resources/views/myPage/MyPage.blade.php
+++ resources/views/myPage/MyPage.blade.php
@@ -22,26 +22,26 @@
         </div>
     </header>
     <main>
-        <div class="grid gap-y-8 mt-36 mx-24">
+        <div class="grid gap-y-8 mt-28 sm:mt-32 mx-8 sm:mx-12 md:mx-24">
             <div class="flex items-center">
-                <img class="w-20 mr-10" src="../../../img/icon.svg">
+                <img class="w-16 sm:w-18 md:w-20 mr-8 sm:mr-10" src="../../../img/icon.svg">
                 <div class="grid gap-y-2">
-                    <p class="text-2xl">名前</p>
-                    <p class="text-2xl">パスワード</p>
-                    <p class="text-1xl">メールアドレス</p>
+                    <p class="text-xl sm:text-2xl">名前</p>
+                    <p class="text-xl sm:text-2xl">パスワード</p>
+                    <p class="text-l sm:text-1xl">メールアドレス</p>
                 </div>
             </div>
             <div class="flex place-items-start">
-                <img class="ml-1 w-8 mr-10" src="../../../img/tags.svg">
-                <ol class=" text-2xl flex flex-wrap gap-4 underline underline-offset-2">
+                <img class="ml-1 w-6 sm:w-8 mr-6 sm:mr-8 md:mr-10" src="../../../img/tags.svg">
+                <ol class=" text-xl sm:text-2xl flex flex-wrap gap-4 underline underline-offset-2">
                     <li>#タグ1</li>
                     <li>#タグ2</li>
                     <li>#タグ3</li>
                 </ol>
             </div>
             <a class="flex items-center" href="../logOut/">
-                <img class="w-10 mr-8" src="../../../img/logout.svg">
-                <div class="text-2xl text-rose-400">ログアウト</div>
+                <img class="w-8 sm:w-10 mr-4 sm:mr-6 md:mr-8" src="../../../img/logout.svg">
+                <div class="text-xl sm:text-2xl text-rose-400">ログアウト</div>
             </a>
         </div>
     </main>
