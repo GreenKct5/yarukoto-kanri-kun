2306e888fb3c51e34d13f3c595a674c373fb7b11
n00bdy-noname <n00bdy.noname@gmail.com>
2024-07-25 07:12:09 +0900

    add: ログアウトを確認するページのファイルを追加

---
+++ resources/views/logOut/LogOut.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @vite('resources/css/app.css')
+    <title>ログアウト</title>
+    <link rel="stylesheet" href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap">
+</head>
+
+<body class="bg-gray-100">
+    <header class="fixed top-0 left-0 w-full bg-blue-100 shadow-md z-10">
+        <div class="container mx-auto py-2 flex justify-between items-center">
+            <button class="flex"><a href="../myPage/"><img class="ml-10" src="../../../img/back.svg"></a></button>
+            <nav>
+                <div class="flex">
+                    <div class="text-xl flex items-center">ログアウト</div>
+                    <img class="object-scale-down h-16 w-16 mx-6" src="../../../img/yakan1.svg">
+                </div>
+            </nav>
+        </div>
+    </header>
+</body>
