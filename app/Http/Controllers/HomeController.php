5396a9e21b4d134190e81b1623810ed310d9a653
kototo522 <huyejiben@gmail.com>
2024-07-24 23:49:24 +0900

    add: feature/issue/#62

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UsersGroup;
+use App\Models\User;
+use App\Models\Todo;
 use App\Models\Group;
 use App\Models\Subject;
-use App\Models\Todo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -21,6 +24,12 @@ class HomeController extends Controller
 
     public function home()
     {
+        $user = Auth::user();
+        $userGroups = UsersGroup::with(['user', 'group'])
+            ->where('user_id', $user->id)
+            ->get();
+
+        // TODO: やまへ，userGroupsのtodoだけ表示させて欲しいです頼んだ
         $todos = Todo::with('group', 'subject')->get();
         $subjects = Subject::all()->keyBy('id'); // SubjectをIDでキー付けして取得
         $groups = Group::with('subjects')->get()->keyBy('id'); // グループと関連するsubjectsを取得
